77520b622a36aa6c1d258fc0c72579b23305bfb1
Onur D <contact@dinony.com>
2018-01-22 15:43:26 +0100

    Prep Vec2 and CSSPos classes

--- src/vec.js
+++ src/vec.js
@@ -9,3 +9,46 @@ export function sub(p1, p2) {
 export function vec2css(p) {
   return {left: `${p.x}px`, top: `${p.y}px`}
 }
+
+const cssPx = x => `${x}px`
+const parseCSSPx = cssPxStr => {
+
+}
+
+export class CSSPos {
+  constructor(left, top) {
+    this.left = cssPx(left)
+    this.top = cssPx(top)
+  }
+
+  static fromVec2(vec2) {
+    return new CSSPos(vec2.x, vec2.y)
+  }
+
+  toVec2() {
+    return new Vec2(parseCSSPx(this.left), parseCSSPx(this.top))
+  }
+}
+
+export class Vec2 {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  add(b) {
+    return new Vec2(this.x+b.x, this.y+b.y)
+  }
+
+  sub(b) {
+    return new Vec2(this.x-b.x, this.y-b.y)
+  }
+
+  static add(a, b) {
+    return new Vec2(a.x+b.x, a.y+b.y)
+  }
+
+  static sub(a, b) {
+    return new Vec2(a.x-b.x, a.y-b.y)
+  }
+}

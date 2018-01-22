6831e94787ce00e73823144c57fbfce43b452295
Onur D <contact@dinony.com>
2018-01-22 18:31:40 +0100

    Bubble up onMouseenter

--- src/item/item.js
+++ src/item/item.js
@@ -1,15 +1,34 @@
 import './item.scss'
 
 export default {
-  functional: true,
-  render(h, context) {
-    const slots = context.slots()
+  props: {
+    item: {
+      required: true
+    },
+    index: {
+      type: Number,
+      required: true
+    },
+    isSelected: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    emitOnEnter() {
+      this.$emit('enter', this.index)
+    }
+  },
+  render() {
     const params = {
       class: {
         'mo-dndItem': true,
-        'mo-dndItemSelected': context.props.isSelected
+        'mo-dndItemSelected': this.isSelected
+      },
+      on: {
+        mouseenter: this.emitOnEnter
       }
     }
-    return <div {...params}>{slots.default}</div>
+    return <div {...params}>{this.$slots.default}</div>
   }
 }

01870ed04048ec8f2c9a99e6e6aef5e4039785b5
Onur D <contact@dinony.com>
2018-01-24 13:58:11 +0100

    Impl drop on same context

--- src/drop.js
+++ src/drop.js
@@ -1,3 +1,49 @@
+export class DropContext {
+  constructor(container, updateFn) {
+    this.container = container
+    this.updateFn = updateFn
+  }
+}
+
+export class DropResult {
+  constructor(sourceResult, targetResult, sameContext, needsUpdate) {
+    this.source = sourceResult
+    this.target = targetResult
+    this.sameContext = sameContext
+    this.needsUpdate = needsUpdate
+  }
+}
+
 export function drop(dragState) {
-  return null
+  const ds = dragState
+  const sc = ds.sourceContext
+  const tc = ds.targetContext
+
+  const selfDrop = () => ds.sameContext && sc.index === tc.index
+
+  if(ds.sameContext && !selfDrop()) {
+    if(sc.index < tc.index) {
+
+    } else {
+
+    }
+  } else if(!ds.sameContext) {
+    const newSrc = sc.options.allowItemRemoval ?
+      sc.container.slice(0, sc.index).concat(sc.container.slice(sc.index+1)):
+      sc.container.slice()
+
+    const newTrg = tc.container.slice(0, tc.index)
+      .concat(sc.item)
+      .concat(tc.container.slice(tc.index+1))
+
+    return new DropResult(
+      new DropContext(newSrc, sc.updateFn),
+      new DropContext(newTrg, tc.updateFn),
+      ds.sameContext, true)
+  } else {
+    return new DropResult(
+      new DropContext(sc.container.slice(), sc.updateFn),
+      new DropContext(tc.container.slice(), tc.updateFn),
+      ds.sameContext, false)
+  }
 }

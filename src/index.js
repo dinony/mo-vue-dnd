61efed05004a3dda2c9da2288a4a865dd476d194
Onur D <contact@dinony.com>
2018-01-24 18:09:03 +0100

    Adapt exports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export {default as DnDContext} from './context/context'
-export {default as DnDItems, DnDOptions} from './items/items'
+export {default as DnDItems} from './items/items'
+export {default as DnDOptions} from './items/dndOptions'
 export {default as DnDHandle} from './handle/handle'

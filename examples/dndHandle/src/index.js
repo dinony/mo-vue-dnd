15c830e82277279375c6db04bb0ca9ea2bf8eb11
Onur D <contact@dinony.com>
2018-01-24 18:05:09 +0100

    Add handle example

---
+++ examples/dndHandle/src/index.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+
+import {
+  DnDContext, DnDItems,
+  DnDOptions, DnDHandle
+} from 'mo-vue-dnd'
+
+import './index.scss'
+
+const leftOptions = new DnDOptions(true, true, true)
+const rightOptions = new DnDOptions(true, false, true)
+
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      left: [1, 2, 3, 4, 5],
+      right: ['A', 'B', 'C', 'D']
+    }
+  },
+  methods: {
+    updateLeft(updateLeft) {
+      this.left = updateLeft
+    },
+    updateRight(updateRight) {
+      this.right = updateRight
+    }
+  },
+
+  render() {
+    const renderDnDItem = props => (
+      <div class="dndItem">
+        Item: {props.item}
+      </div>)
+    // const renderDnDItem = props => (
+    //   <div class="dndItem">
+    //     <DnDHandle container={props.container}>
+    //       <div class="dndItemHandle"></div>
+    //     </DnDHandle>
+    //     <div class="dndItemContent">Item: {props.item}</div>
+    //   </div>)
+    const slots = {default: renderDnDItem}
+
+    return (
+      <DnDContext debug={true} scopedSlots={slots}>
+        <div class="container">
+          <div class="dndWrapper">
+            <DnDItems items={this.left} onUpdate={this.updateLeft} options={leftOptions} scopedSlots={slots} />
+            <DnDItems items={this.right} onUpdate={this.updateRight} options={rightOptions} scopedSlots={slots}/>
+          </div>
+          <div class="dbWrapper">
+            <pre class="db">Left: {JSON.stringify(this.left, 2)}</pre>
+            <pre class="db">Right: {JSON.stringify(this.right, 2)}</pre>
+          </div>
+        </div>
+      </DnDContext>)
+  }
+})

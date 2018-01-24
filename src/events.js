763298ee2fe1ed8e5eb92a94de6bb83c0136f592
Onur D <contact@dinony.com>
2018-01-24 18:09:21 +0100

    Add handle mousedown event const

--- src/events.js
+++ src/events.js
@@ -10,3 +10,11 @@ export class DnDItemSelectPayload {
 export const DND_ITEM_SELECTED = 'MO_DND_ITEM_SELECTED'
 
 export const DND_ITEM_UNSELECTED = 'MO_DND_ITEM_UNSELECTED'
+
+export const DND_HANDLE_MD = 'MO_DND_HANDLE_MD'
+export class DnDHandleMdPayload {
+  constructor(event, container) {
+    this.event = event
+    this.container = container
+  }
+}

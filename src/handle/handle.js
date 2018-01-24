06e4978544f86f47d5bb10a97c174bd85d590426
Onur D <contact@dinony.com>
2018-01-24 18:08:37 +0100

    Prep handle component

--- src/handle/handle.js
+++ src/handle/handle.js
@@ -1,6 +1,19 @@
+import './handle.scss'
+import bus from '../bus'
+import {DND_HANDLE_MD, DnDHandleMdPayload} from '../events'
+
+function onMousedown(event, container) {
+  // Just left button clicks
+  if(event.button !== 0) {return}
+  bus.$emit(DND_HANDLE_MD, new DnDHandleMdPayload(event, container))
+}
+
 export default {
   functional: true,
   render(h, context) {
-    return <div class="mo-dndHandle">{context.slots().default}</div>
+    return (
+      <div class="mo-dndHandle" onMousedown={ev => onMousedown(ev, context.props.container)}>
+        {context.slots().default}
+      </div>)
   }
 }

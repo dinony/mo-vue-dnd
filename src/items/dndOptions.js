612076a7a010041cb1110a232f4604fc8477adc3
Onur D <contact@dinony.com>
2018-01-25 13:53:17 +0100

    Allow clone method to be configable

--- src/items/dndOptions.js
+++ src/items/dndOptions.js
@@ -1,10 +1,14 @@
+import {cloneItemFn as defCloneFn} from './clone'
+
 export default class DnDOptions {
   constructor(
     allowSameContainer=true,
     allowItemRemoval=false,
-    wrapDnDHandle=true) {
+    wrapDnDHandle=true,
+    cloneItemFn=defCloneFn) {
     this.allowSameContainer = allowSameContainer
     this.allowItemRemoval = allowItemRemoval
     this.wrapDnDHandle = wrapDnDHandle
+    this.cloneItemFn = cloneItemFn
   }
 }

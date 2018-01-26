ec0bd53ed74dc285c93643606b3e3701a92a6c3b
Onur D <contact@dinony.com>
2018-01-26 14:19:54 +0100

    Allow simpler permissions init

--- src/items/dndOptions.js
+++ src/items/dndOptions.js
@@ -1,15 +1,26 @@
 import {cloneItemFn as defCloneFn} from './clone'
-import DnDPermissions from './dndPermissions'
+import {default as DnDPermissions, getPermissions} from './dndPermissions'
 
 export default class DnDOptions {
   constructor(
     allowItemRemoval=true,
     wrapDnDHandle=true,
-    permissions=new DnDPermissions(),
+    permissions=[null, null],
     cloneItemFn=defCloneFn) {
     this.allowItemRemoval = allowItemRemoval
     this.wrapDnDHandle = wrapDnDHandle
     this.cloneItemFn = cloneItemFn
-    this.permissions = permissions
+    this.perms = permissions
+  }
+
+  set permissions(perms) {
+    if(perms.length !== 2) {return}
+    const _in = perms[0]
+    const out = perms[1]
+    this.perms = getPermissions(_in, out)
+  }
+
+  get permissions() {
+    return this.perms
   }
 }

c6eb47a5f60b601e1ecaff4555dc7b11bd64631c
Onur D <contact@dinony.com>
2018-01-26 17:50:28 +0100

    Fix css filename

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')
 const postcss = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
+const rename = require('gulp-rename')
 
 gulp.task('scss', function () {
   return gulp.src('./src/index.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([autoprefixer()]))
-    .pipe(gulp.dest('./dist'))
+    .pipe(rename('mo-vue-dnd.css'))
+    .pipe(gulp.dest('./dist/'))
 })

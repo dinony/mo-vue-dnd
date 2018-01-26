f9bac307d870e408070061bdd8aa3a99fda24262
Onur D <contact@dinony.com>
2018-01-26 17:06:25 +0100

    Use shorter string conts

--- src/events.js
+++ src/events.js
@@ -1,4 +1,4 @@
-export const DND_ITEM_SELECT = 'MO_DND_ITEM_SELECT'
+export const DND_ITEM_SELECT = 'IS'
 export class DnDItemSelectPayload {
   constructor(event, elem, context) {
     this.event = event
@@ -7,11 +7,11 @@ export class DnDItemSelectPayload {
   }
 }
 
-export const DND_ITEM_SELECTED = 'MO_DND_ITEM_SELECTED'
+export const DND_ITEM_SELECTED = 'ISD'
 
-export const DND_ITEM_UNSELECTED = 'MO_DND_ITEM_UNSELECTED'
+export const DND_ITEM_UNSELECTED = 'IUD'
 
-export const DND_HANDLE_MD = 'MO_DND_HANDLE_MD'
+export const DND_HANDLE_MD = 'MD'
 export class DnDHandleMdPayload {
   constructor(event, container) {
     this.event = event

9a9820f1eeb68c5d47e8b4dca534b9f84b0cc819
Onur D <contact@dinony.com>
2018-01-26 09:48:56 +0100

    Adjust default options

--- src/items/dndOptions.js
+++ src/items/dndOptions.js
@@ -3,7 +3,7 @@ import {cloneItemFn as defCloneFn} from './clone'
 export default class DnDOptions {
   constructor(
     allowSameContainer=true,
-    allowItemRemoval=false,
+    allowItemRemoval=true,
     wrapDnDHandle=true,
     cloneItemFn=defCloneFn) {
     this.allowSameContainer = allowSameContainer

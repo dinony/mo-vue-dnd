055fe25f3ea016eef5c41bb279dfe47d700c11ba
Onur D <contact@dinony.com>
2018-01-26 10:53:24 +0100

    Adjust variables

--- examples/groups/src/index.js
+++ examples/groups/src/index.js
@@ -8,40 +8,40 @@ new Vue({
   el: '#app',
   data() {
     return {
-      tl: ['TL1', 'TL2', 'TL3'],
-      tr: ['TR1', 'TR2', 'TR3', 'TR4'],
-      bl: ['BL1', 'BL2'],
-      br: ['BR1', 'BR2', 'BR3']
+      a: ['a1', 'a2', 'a3'],
+      b: ['b1', 'b2', 'b3', 'b4'],
+      c: ['c1', 'c2'],
+      d: ['d1', 'd2', 'd3']
     }
   },
   methods: {
     updateTopLeft(data) {
-      this.tl = data
+      this.a = data
     },
     updateTopRight(data) {
-      this.tr = data
+      this.b = data
     },
     updateBottomLeft(data) {
-      this.bl = data
+      this.c = data
     },
     updateButtomRight(data) {
-      this.br = data
+      this.d = data
     }
   },
   render() {
-    const renderItem = props => <div class="dndItem">Item: {props.item}</div>
+    const renderItem = props => <div class="dndItem">{props.item}</div>
     const slots = {default: renderItem}
 
     return (
       <DnDContext scopedSlots={slots}>
         <div class="container">
           <div class="row">
-            <DnDItems items={this.tl} onUpdate={this.updateTopLeft} scopedSlots={slots}/>
-            <DnDItems items={this.tr} onUpdate={this.updateTopRight} scopedSlots={slots}/>
+            <DnDItems items={this.a} group="a" onUpdate={this.updateTopLeft} scopedSlots={slots}/>
+            <DnDItems items={this.b} group="b" onUpdate={this.updateTopRight} scopedSlots={slots}/>
           </div>
           <div class="row">
-            <DnDItems items={this.bl} onUpdate={this.updateBottomLeft} scopedSlots={slots}/>
-            <DnDItems items={this.br} onUpdate={this.updateButtomRight} scopedSlots={slots}/>
+            <DnDItems items={this.c} group="c" onUpdate={this.updateBottomLeft} scopedSlots={slots}/>
+            <DnDItems items={this.d} group="d" onUpdate={this.updateButtomRight} scopedSlots={slots}/>
           </div>
         </div>
       </DnDContext>

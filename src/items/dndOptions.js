9dc26a3658130dbb896da6008b5cf3a30115f669
Onur D <contact@dinony.com>
2018-01-26 14:05:23 +0100

    Rm unnecessary option

--- src/items/dndOptions.js
+++ src/items/dndOptions.js
@@ -3,12 +3,10 @@ import DnDPermissions from './dndPermissions'
 
 export default class DnDOptions {
   constructor(
-    allowSameContainer=true,
     allowItemRemoval=true,
     wrapDnDHandle=true,
     permissions=new DnDPermissions(),
     cloneItemFn=defCloneFn) {
-    this.allowSameContainer = allowSameContainer
     this.allowItemRemoval = allowItemRemoval
     this.wrapDnDHandle = wrapDnDHandle
     this.cloneItemFn = cloneItemFn

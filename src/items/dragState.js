8df5fe1c8de7550844466f9ef432c77f1ad39bb9
Onur D <contact@dinony.com>
2018-01-30 19:09:29 +0100

    Add insertBefore flag

--- src/items/dragState.js
+++ src/items/dragState.js
@@ -1,7 +1,8 @@
 export default class DragState {
-  constructor(sourceContext, targetContext, isSameContext) {
+  constructor(sourceContext, targetContext, isSameContext, insertBefore) {
     this.sourceContext = sourceContext
     this.targetContext = targetContext
     this.sameContext = isSameContext
+    this.insertBefore = insertBefore
   }
 }

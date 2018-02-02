184cd1137b985e8ed5211e37136b42508934934a
Onur D <contact@dinony.com>
2018-02-02 14:29:44 +0100

    Add resultFn to drag state

--- src/items/dragState.js
+++ src/items/dragState.js
@@ -1,8 +1,9 @@
 export default class DragState {
-  constructor(sourceContext, targetContext, sameContext, insertBefore) {
+  constructor(sourceContext, targetContext, sameContext, insertBefore, resultFn) {
     this.sourceContext = sourceContext
     this.targetContext = targetContext
     this.sameContext = sameContext
     this.insertBefore = insertBefore
+    this.resultFn = resultFn
   }
 }

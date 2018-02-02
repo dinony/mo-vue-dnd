77666fab57031ac245f297d44383d46ddb40fa0a
Onur D <contact@dinony.com>
2018-02-02 13:30:12 +0100

    Rm var

--- src/items/dropResult.js
+++ src/items/dropResult.js
@@ -1,8 +1,7 @@
 export default class DropResult {
-  constructor(sourceResult, targetResult, sameContext, needsUpdate) {
+  constructor(sourceResult, targetResult, sameContext) {
     this.source = sourceResult
     this.target = targetResult
     this.sameContext = sameContext
-    this.needsUpdate = needsUpdate
   }
 }

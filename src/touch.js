123167de0e62dd20d5c4eb83fe540d94089e9ed1
Onur D <contact@dinony.com>
2018-02-07 17:21:37 +0100

    Check touch event name

--- src/touch.js
+++ src/touch.js
@@ -16,9 +16,32 @@ const microsoft = {
   mousemove: 'MSPointerMove'
 }
 
+export class InvalidTouchEventNameException extends Error {
+  constructor(message) {
+    super(message)
+  }
+}
+
+const invalidEventNameErrMsg = key =>
+  (`
+   Event name: \`${key}\` is not recognized as a touch event.
+   Please use a valid event name ('mousedown'|'mousemove'|'mouseup').
+  `)
+
+function checkTouchEventName(event) {
+  if(event === 'mousedown' || event === 'mousemove' || event === 'mouseup') {
+    return
+  } else {
+    throw new InvalidTouchEventNameException(invalidEventNameErrMsg(event))
+  }
+}
+
+
 // Reference:
 // https://github.com/bevacqua/dragula/blob/master/dragula.js#L495
 export default function attachTouchy(obj, event, fn) {
+  checkTouchEventName(event)
+
   if (global.navigator.pointerEnabled) {
     obj[pointers[event]] = fn
   } else if (global.navigator.msPointerEnabled) {

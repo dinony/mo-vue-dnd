e9b9561bc25764586602663ed619ccb945900a78
Onur D <contact@dinony.com>
2018-02-07 16:19:42 +0100

    Add deploy script for nested example

--- examples/nested/webpack.config.js
+++ examples/nested/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   ],
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/examples/nested/dist/',
+    publicPath: 'examples/nested/dist/',
     filename: 'build.js',
   },
   module: {
---
+++ tools/deploy.sh
@@ -0,0 +1,2 @@
+scp -r examples/nested/dist dinony@dinony.com:~/www/static/projects/vue-dnd/dist
+scp examples/nested/index.html dinony@dinony.com:~/www/static/projects/vue-dnd/index.html

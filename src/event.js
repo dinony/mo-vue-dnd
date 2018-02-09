4a2822bdcecfc4d1dae34fcd3037aa2c7b5e12e7
Onur D <contact@dinony.com>
2018-02-09 16:25:27 +0100

    Also attach client coords

--- src/event.js
+++ src/event.js
@@ -6,13 +6,17 @@ export function getEventCoords(event) {
     if(touch) {
       return {
         'pageX': touch.pageX,
-        'pageY': touch.pageY
+        'pageY': touch.pageY,
+        'clientX': touch.clientX,
+        'clientY': touch.clientY
       }
     }
   } else if(event instanceof MouseEvent) {
     return {
       'pageX': event.pageX,
-      'pageY': event.pageY
+      'pageY': event.pageY,
+      'clientX': event.clientX,
+      'clientY': event.clientY
     }
   }
   return null

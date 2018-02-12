f47e7e4874f048fa37e736a56ad03b699d54bd4f
Onur D <contact@dinony.com>
2018-02-12 16:27:52 +0100

    Rm unneeded

--- src/events.js
+++ src/events.js
@@ -1,51 +1,10 @@
 export const DND_ITEM_TRACED = 'IT'
 
-// ItemTracedPayload
-export class ItemTracedPl {
-  constructor(event, traceResult) {
-    this.ev = event
-    this.tRes = traceResult
-  }
-}
-
 export const DND_ITEM_SELECT = 'IS'
-// export class ItemSelectPayload {
-//   constructor(event, elem, itemContext) {
-//     this.event = event
-//     this.elem = elem
-//     this.itemContext = itemContext
-//   }
-// }
-
 export const DND_ITEM_SELECTED = 'ISD'
-
 export const DND_ITEM_UNSELECTED = 'IUD'
 
-// export const DND_REQUEST_ITEM = 'RI'
-// export const DND_REQUESTED_ITEM = 'RID'
-
-// export const DND_REQUEST_TARGET = 'RT'
-// export const DND_REQUESTED_TARGET = 'RTD'
-
-// export const DND_TARGET_SELECT = 'TS'
-
-// export class TargetSelectPayload {
-//   constructor(targetElement) {
-//     this.targetElement = targetElement
-//   }
-// }
-
 export const DND_TARGET_SELECTED = 'TSD'
-// export const DND_TARGET_UNSELECT = 'TU'
 export const DND_TARGET_UNSELECTED = 'TUD'
 
-// export const DND_TARGET_ITEM_CONTEXT = 'TIC'
-
-// export class TargetItemContextPayload {
-//   constructor(event, targetElem, itemElem, itemIndex) {
-//     this.event = event
-//     this.targetElem = targetElem
-//     this.itemElem = itemElem
-//     this.itemIndex = itemIndex
-//   }
-// }
+export const DND_MOVE_TRACE = 'MT'

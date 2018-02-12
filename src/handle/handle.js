3decf8af8ed2ecc1b61722bf361909f4a4ccb301
Onur D <contact@dinony.com>
2018-02-12 11:50:42 +0100

    Do not reg event handlers in handle component. Just use as simple wrapper

--- src/handle/handle.js
+++ src/handle/handle.js
@@ -1,28 +1,6 @@
-import bus from '../bus'
-import {default as attachTouchy, isTouch} from '../touch'
-import {DND_HANDLE_MD, HandleMdPayload} from '../events'
-
-function onMousedown(event, componentContext) {
-  if(isTouch(event)) {
-    if(event.touches.length !== 1) {return}
-  } else {
-    // Just left button clicks
-    if(event.button !== 0) {return}
-  }
-
-  event.stopPropagation()
-  bus.$emit(DND_HANDLE_MD, new HandleMdPayload(event, componentContext))
-}
-
 export default {
   functional: true,
   render(h, context) {
-    const listeners = {on:{}}
-    attachTouchy(listeners.on, 'mousedown', ev => onMousedown(ev, context.props.componentContext))
-
-    return (
-      <div class="mo-dndHandle" {...listeners}>
-        {context.slots().default}
-      </div>)
+    return <div class="mo-dndHandle">{context.slots().default}</div>
   }
 }

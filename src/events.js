904ba7dfdb52673336e485cd7479e4a58e3d5b79
Onur D <contact@dinony.com>
2018-02-12 14:31:58 +0100

    Tmp comment out unused stuff

--- src/events.js
+++ src/events.js
@@ -21,31 +21,31 @@ export const DND_ITEM_SELECTED = 'ISD'
 
 export const DND_ITEM_UNSELECTED = 'IUD'
 
-export const DND_REQUEST_ITEM = 'RI'
-export const DND_REQUESTED_ITEM = 'RID'
+// export const DND_REQUEST_ITEM = 'RI'
+// export const DND_REQUESTED_ITEM = 'RID'
 
-export const DND_REQUEST_TARGET = 'RT'
-export const DND_REQUESTED_TARGET = 'RTD'
+// export const DND_REQUEST_TARGET = 'RT'
+// export const DND_REQUESTED_TARGET = 'RTD'
 
-export const DND_TARGET_SELECT = 'TS'
+// export const DND_TARGET_SELECT = 'TS'
 
-export class TargetSelectPayload {
-  constructor(targetElement) {
-    this.targetElement = targetElement
-  }
-}
+// export class TargetSelectPayload {
+//   constructor(targetElement) {
+//     this.targetElement = targetElement
+//   }
+// }
 
-export const DND_TARGET_SELECTED = 'TSD'
-export const DND_TARGET_UNSELECT = 'TU'
-export const DND_TARGET_UNSELECTED = 'TUD'
+// export const DND_TARGET_SELECTED = 'TSD'
+// export const DND_TARGET_UNSELECT = 'TU'
+// export const DND_TARGET_UNSELECTED = 'TUD'
 
-export const DND_TARGET_ITEM_CONTEXT = 'TIC'
+// export const DND_TARGET_ITEM_CONTEXT = 'TIC'
 
-export class TargetItemContextPayload {
-  constructor(event, targetElem, itemElem, itemIndex) {
-    this.event = event
-    this.targetElem = targetElem
-    this.itemElem = itemElem
-    this.itemIndex = itemIndex
-  }
-}
+// export class TargetItemContextPayload {
+//   constructor(event, targetElem, itemElem, itemIndex) {
+//     this.event = event
+//     this.targetElem = targetElem
+//     this.itemElem = itemElem
+//     this.itemIndex = itemIndex
+//   }
+// }

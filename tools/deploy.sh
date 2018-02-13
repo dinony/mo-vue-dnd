f9ed6c941453d7959e9611186ed067e828cceaee
Onur D <contact@dinony.com>
2018-02-13 11:26:53 +0100

    Add cleanup step

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,2 +1,7 @@
+#!/bin/sh
+# Cleanup
+ssh dinony@dinony.com 'cd www/static/projects/vue-dnd; rm -rf dist'
+ssh dinony@dinony.com 'cd www/static/projects/vue-dnd; ls'
+# Deploy
 scp -r examples/nested/dist dinony@dinony.com:~/www/static/projects/vue-dnd/dist
 scp examples/nested/index.html dinony@dinony.com:~/www/static/projects/vue-dnd/index.html

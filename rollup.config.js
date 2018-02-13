aff9925c741bf0a23868a925c66a74dbf7cc2ae7
Onur D <contact@dinony.com>
2018-02-13 20:27:34 +0100

    Adapt rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,26 @@
 import babel from 'rollup-plugin-babel'
 import uglify from 'rollup-plugin-uglify'
+import filesize from 'rollup-plugin-filesize'
+import license from 'rollup-plugin-license'
+
+const pkg = require('./package.json')
+
+const banner = `
+mo-vue-dnd v${pkg.version}
+(c) 2018 ${pkg.author}
+License: ${pkg.license}
+`
 
 const configs = {
   plugins: [
     babel({
       exclude: 'node_modules/**'
     }),
-    uglify()
+    uglify(),
+    license({
+      banner,
+    }),
+    filesize()
   ],
   external: ['vue']
 }
@@ -20,7 +34,10 @@ export default [{
   {
     file: 'dist/mo-vue-dnd.umd.js',
     format: 'umd',
-    name: 'mo-vue-dnd'
+    name: 'mo-vue-dnd',
+    globals: {
+      vue: 'Vue'
+    }
   },
   {
     file: 'dist/mo-vue-dnd.esm.js',

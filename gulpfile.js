09ddc097442413becc75c1279de2e1861896846f
Onur D <contact@dinony.com>
2018-02-15 18:50:48 +0100

    Add demo scss task

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,14 @@ const autoprefixer = require('autoprefixer')
 const cssnano = require('cssnano')
 const rename = require('gulp-rename')
 
-gulp.task('scss', function () {
-  return gulp.src('./src/index.scss')
+const scssFn = (inPath, outPath, outFile) => () => {
+  return gulp.src(inPath)
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([autoprefixer(), cssnano()]))
-    .pipe(rename('mo-vue-dnd.css'))
-    .pipe(gulp.dest('./dist/'))
-})
+    .pipe(rename(outFile))
+    .pipe(gulp.dest(outPath))
+}
+
+gulp.task('scss', scssFn('src/index.scss', 'dist/', 'mo-vue-dnd.css'))
+
+gulp.task('nested:scss', scssFn('examples/nested/src/index.scss', 'examples/nested/dist', 'index.css'))
